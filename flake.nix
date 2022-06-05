50965c049616b906707d3891265385cb81adcab9
Peter Rice <peter@peterrice.xyz>
2022-06-05 16:25:38 -0400

    nixos module is system-independent

--- flake.nix
+++ flake.nix
@@ -32,70 +32,70 @@
           drv = packages.podcasts;
           exePath = "/bin/fetch-podcasts";
         };
-
-        nixosModules.default = { config, lib, pkgs, ... }:
-          let
-            cfg = config.services.podcasts;
-            podcasts = self.outputs.packages."${pkgs.system}".podcasts;
-            penv = podcasts.dependencyEnv;
-            fetch-podcasts = "${podcasts}/bin/fetch-podcasts";
-            stateDirectory = "/var/lib/podcasts/";
-            commonServiceConfig = {
-              DynamicUser = true;
-              User = "podcasts";
-              Group = "podcasts";
-              StateDirectory = "podcasts";
-              WorkingDirectory = cfg.podcastDir;
-              ProtectHome = "tmpfs";
-            };
-          in
-          {
-            options = {
-              services.podcasts = with lib; {
-                enableFetch = mkEnableOption "fetch-podcasts";
-                enableServe = mkEnableOption "serve-podcasts";
-                podcastDir = mkOption {
-                  type = types.str;
-                  default = stateDirectory + "podcasts";
-                };
-                startAt = mkOption {
-                  type = types.str;
-                  default = "daily";
-                };
+      };
+    in
+    with utils.lib; eachSystem defaultSystems out // {
+      nixosModules.default = { config, lib, pkgs, ... }:
+        let
+          cfg = config.services.podcasts;
+          podcasts = self.outputs.packages."${pkgs.system}".podcasts;
+          penv = podcasts.dependencyEnv;
+          fetch-podcasts = "${podcasts}/bin/fetch-podcasts";
+          stateDirectory = "/var/lib/podcasts/";
+          commonServiceConfig = {
+            DynamicUser = true;
+            User = "podcasts";
+            Group = "podcasts";
+            StateDirectory = "podcasts";
+            WorkingDirectory = cfg.podcastDir;
+            ProtectHome = "tmpfs";
+          };
+        in
+        {
+          options = {
+            services.podcasts = with lib; {
+              enableFetch = mkEnableOption "fetch-podcasts";
+              enableServe = mkEnableOption "serve-podcasts";
+              podcastDir = mkOption {
+                type = types.str;
+                default = stateDirectory + "podcasts";
               };
-            };
-            config = lib.mkIf (cfg.enableFetch || cfg.enableServe) {
-              systemd.services.fetch-podcasts = {
-                enable = cfg.enableFetch;
-                script = "${fetch-podcasts} ${cfg.podcastDir} ${stateDirectory}";
-                serviceConfig = commonServiceConfig // {
-                  Type = "oneshot";
-                  BindPaths = [ cfg.podcastDir ];
-                };
-                startAt = cfg.startAt;
-              };
-              systemd.services.serve-podcasts = {
-                enable = cfg.enableServe;
-                serviceConfig = commonServiceConfig // {
-                  BindReadOnlyPaths = [ cfg.podcastDir ];
-                  # TODO get port from cfg
-                  ExecStart = ''
-                    ${pkgs.python3Packages.gunicorn}/bin/gunicorn -b 0.0.0.0:5998 podcasts.serve:app
-                  '';
-                };
-                environment =
-                  {
-                    # TODO unify with cli args
-                    PODCASTS_ANNEX_DIR = cfg.podcastDir;
-                    PODCASTS_DATA_DIR = stateDirectory;
-                    PYTHONPATH = "${penv}/${penv.sitePackages}";
-                  };
-                wantedBy = [ "multi-user.target" ];
-                after = [ "network.target" ] ++ lib.optional cfg.enableFetch "fetch-podcasts.timer";
+              startAt = mkOption {
+                type = types.str;
+                default = "daily";
               };
             };
           };
-      };
-    in with utils.lib; eachSystem defaultSystems out;
-
+          config = lib.mkIf (cfg.enableFetch || cfg.enableServe) {
+            systemd.services.fetch-podcasts = {
+              enable = cfg.enableFetch;
+              script = "${fetch-podcasts} ${cfg.podcastDir} ${stateDirectory}";
+              serviceConfig = commonServiceConfig // {
+                Type = "oneshot";
+                BindPaths = [ cfg.podcastDir ];
+              };
+              startAt = cfg.startAt;
+            };
+            systemd.services.serve-podcasts = {
+              enable = cfg.enableServe;
+              serviceConfig = commonServiceConfig // {
+                BindReadOnlyPaths = [ cfg.podcastDir ];
+                # TODO get port from cfg
+                ExecStart = ''
+                  ${pkgs.python3Packages.gunicorn}/bin/gunicorn -b 0.0.0.0:5998 podcasts.serve:app
+                '';
+              };
+              environment =
+                {
+                  # TODO unify with cli args
+                  PODCASTS_ANNEX_DIR = cfg.podcastDir;
+                  PODCASTS_DATA_DIR = stateDirectory;
+                  PYTHONPATH = "${penv}/${penv.sitePackages}";
+                };
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ] ++ lib.optional cfg.enableFetch "fetch-podcasts.timer";
+            };
+          };
+        };
+    };
 }
