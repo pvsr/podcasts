0ff986b8561e7af3081acff6a4770e18b5202fef
Peter Rice <peter@peterrice.xyz>
2022-07-20 02:04:29 -0400

    set up sqlalchemy connection automatically

--- podcasts/serve.py
+++ podcasts/serve.py
@@ -3,39 +3,38 @@ from pathlib import Path
 from typing import Optional
 
 from flask import Flask, render_template, send_from_directory
-
 from flask_httpauth import HTTPBasicAuth
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc, select
-from sqlalchemy.orm import Session
 from werkzeug.security import check_password_hash
 
-from podcasts.db import PodcastDb, UserDb, create_database
+from podcasts.db import PodcastDb, UserDb
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f'sqlite:///{environ.get("PODCASTS_DATA_DIR", "")}/podcasts.sqlite'
+db = SQLAlchemy(app)
 
 
 @auth.verify_password
 def verify_password(username, password) -> Optional[UserDb]:
-    engine = create_database()
-    with Session(engine) as session:
-        user = session.scalar(select(UserDb).filter_by(name=username))
-        if user and check_password_hash(user.password, password):
-            return user
-        return None
+    user = db.session.scalar(select(UserDb).filter_by(name=username))
+    if user and check_password_hash(user.password, password):
+        return user
+    return None
 
 
 @app.route("/")
 @auth.login_required
 def home():
-    engine = create_database()
-    with Session(engine) as session:
-        podcasts = session.scalars(
-            select(PodcastDb).order_by(desc(PodcastDb.last_ep))
-        ).all()
-        return render_template(
-            "podcasts.html", podcasts=podcasts, updated="todo", base_url=""
-        )
+    podcasts = db.session.scalars(
+        select(PodcastDb).order_by(desc(PodcastDb.last_ep))
+    ).all()
+    return render_template(
+        "podcasts.html", podcasts=podcasts, updated="todo", base_url=""
+    )
 
 
 @app.route("/<path:path>")
