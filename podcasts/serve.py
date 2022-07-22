42e7835ab6c1382b09ee0196a96a1f539939abfb
Peter Rice <peter@peterrice.xyz>
2022-07-21 21:46:43 -0400

    send last-modified for index
    even though it doesn't seem to respect if-modified-since for some reason

--- podcasts/serve.py
+++ podcasts/serve.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import Optional
 from urllib.parse import urlparse
 
-from flask import render_template, send_from_directory
+from flask import make_response, render_template, send_from_directory
 from flask_httpauth import HTTPBasicAuth
 from sqlalchemy import desc, select
 from werkzeug.security import check_password_hash
@@ -33,13 +33,15 @@ def home():
     base_url = urlparse(
         Config.load(Path(environ.get("PODCASTS_DATA_DIR", ""))).base_url
     )
-    last_fetch = max(podcasts, key=lambda p: p.last_fetch).last_fetch_pretty()
-    return render_template(
+    last_podcast = max(podcasts, key=lambda p: p.last_fetch)
+    resp = make_response(render_template(
         "podcasts.html",
         podcasts=podcasts,
-        updated=last_fetch,
+        updated=last_podcast.last_fetch_pretty(),
         auth_url=base_url._replace(netloc=f"{login}@{base_url.netloc}").geturl(),
-    )
+    ))
+    resp.last_modified = last_podcast.last_fetch
+    return resp
 
 
 @app.route("/<path:path>")
